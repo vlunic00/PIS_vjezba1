8c8548b59699fb6b62ee85b5aadb0a14411e2a9d
f <f>
2022-03-15 19:16:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vjezba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
